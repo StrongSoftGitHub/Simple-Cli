947479d87dea82a244d3124a40d2d3416882828c
罗维辉 <lwh1@strongsoft.net>
2018-01-30 09:04:53 +0800

    修改 集成reset.css，用于清除浏览器默认样式

--- template/src/main.js
+++ template/src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+// 清除浏览器默认的样式
+import 'reset.css'
 {{#if_eq cliType 'plugin'}}import VuePlugin from './lib/'
 {{/if_eq}}import App from './App.vue'
 {{#if_eq cliType 'plugin'}}Vue.use(VuePlugin)// 或者传入参数对象 Vue.use(VuePlugin,{})
